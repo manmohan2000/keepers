cf0ea63f0f1422d8608203b5750523eaa071eeac
manmohan2000 <76937755+manmohan2000@users.noreply.github.com>
2023-06-11 13:08:14 +0530

    Initial commit

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+function Foooter(){
+  return(
+     <footer className='footer'>
+       <p>Copyright@2023</p>
+     </footer>
+  );
+}
+export default Foooter;
---
+++ src/Note.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css';
+function Note(){
+  return(
+    <div className='note' >
+      <h1>This is tittle</h1>
+      <p>This is content</p>
+      </div>
+  );
+}
+export default Note;
